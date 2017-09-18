b3ed2e2105f5cd15f4f98b02bec0156409b0e232
Guillermo Lopez <guillermolopez2529@gmail.com>
2017-09-17 21:47:08 -0400

    signup added

--- src/auth/SigninForm.js
+++ src/auth/SigninForm.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const SigninForm = (props) => (
   <form onSubmit={props.authenticate}>
-    <div>
-      <h2> Login</h2>
+      <h2>Sign in</h2>
       <p>Username: <input type="text" name="username" /></p>
       <p>Password: <input type="text" name="password" /></p>
       <button>Login</button>
-    </div>
+      <Link to='/signup'>
+        <button>Signup</button>
+      </Link>
   </form>
 )
 
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,13 +1,49 @@
 import React, { Component } from 'react'
+import SignupForm from '../auth/SignupForm'
+import {
+  gql,
+  graphql
+} from 'react-apollo'
 
 class Signup extends Component {
+  constructor (props) {
+    super(props)
+    this.saveUser = this.saveUser.bind(this)
+  }
+
+  async saveUser (e) {
+    e.preventDefault()
+    const form = e.target
+    const formData = new FormData(form)
+    const { mutate } = this.props
+    const newUser = {
+      fullname: formData.get('fullname'),
+      username: formData.get('username'),
+      email: formData.get('email'),
+      password: formData.get('password')
+    }
+
+    try {
+      await mutate({ variables: { user: newUser } })
+      form.reset()
+    } catch (e) {
+      alert(e.message)
+    }
+  }
+
   render () {
     return (
-      <div>
-        Signup View
-      </div>
+      <SignupForm saveUser={this.saveUser} />
     )
   }
 }
 
-export default Signup
+const saveUserMutataion = gql`
+mutation saveUser($user: newUser) {
+  saveUser(user: $user) {
+    username
+  }
+}
+`
+
+export default graphql(saveUserMutataion)(Signup)
